071c0785e5216aed35c8217993ac1e10d33d2e64
alice <alice.chien100@gmail.com>
2022-08-24 21:14:44 +0800

    complete step_12

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   /x
   validates :name, presence: true
   validates :password, presence: true, format: { with: /[A-Za-z\d]{8,}/, message: " only accepts:length >= 8, at least one digit & lower/upper character" }
-  validates :position, presence: true, format: { with: /[(user)pvio]/ }
+  validates :position, presence: true, inclusion: { in: %w[user supervisor] }
 end
