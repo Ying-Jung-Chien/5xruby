6ee822f86ce04dd21f410d10f0a68399c180942c
alice <alice.chien100@gmail.com>
2022-08-25 13:44:40 +0800

    heroku setting

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root :to => "users#index"
+  root "users#index"
   resources :users
   resources :tasks
   resources :tags
