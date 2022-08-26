ee6f3ed767874ef26ede44d7ac26bdbbee2d0e6a
CY <67591631+ice12031110@users.noreply.github.com>
2022-08-26 13:51:58 +0800

    Merge pull request #8 from Ying-Jung-Chien/step_14
    complete step 14
    
    (cherry picked from commit caf3acf67dcc3fdb832fd19a705c52d334096668)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.order(created_at: :desc)
+    @tasks = Task.order("#{params[:sort]} #{params[:dir]}")
   end
 
   def new
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,19 @@
 module TasksHelper
+  def next_dir
+    if params[:dir].present?
+      params[:dir].eql?('desc') ? 'asc' : 'desc'
+    else
+      'desc'
+    end
+  end
+
+  def priority2text(value)
+    priority = [I18n.t("low"), I18n.t("medium"), I18n.t("high")]
+      priority[value]
+  end
+
+  def status2text(value)
+    status = [I18n.t("solved"), I18n.t("processing"), I18n.t("pending")]
+      status[value]
+  end
 end
