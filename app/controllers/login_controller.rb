2e2b87e0d3e6208319e813dc2cd3af189df63fa4
alice <alice.chien100@gmail.com>
2022-09-01 21:49:19 +0800

    wip

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -6,14 +6,15 @@ class LoginController < ApplicationController
       user = User.find_by(name: params[:login][:name])
       if user && user.authenticate(params[:login][:password])
         session[:user_id] = user.id
-        session[:login] = true
         redirect_to tasks_path, notice: "Logged in successfully"
       else
         # Create an error message.
-        render 'new', notice: "Failed"
+        render 'new', notice: "Incorrect email or password, try again."
       end
     end
   
     def destroy
+      session.delete(:user_id)
+      redirect_to login_path, notice: "Logged out!"
     end
 end
